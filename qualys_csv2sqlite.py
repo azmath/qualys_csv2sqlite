36dc282e800d5b0e5c29fb78f70b136803339774
mutex <iamazmath@gmail.com>
2018-11-01 14:40:38 +0400

    Update qualys_csv2sqlite.py

--- qualys_csv2sqlite.py
+++ qualys_csv2sqlite.py
@@ -11,3 +11,15 @@ cur.execute (''' create table if not exists qualys (ip varchar(20), dns varchar(
 qid int(10), title varchar(100), type varchar(10), severity varchar(10), port int(7), protocol varchar(5), first_detected text, cve_id text,
 vendor_ref text, cvss_base int, threat text, impact text, solution text, exploitable text, malware text, results text, category text )''')
 
+sqldb.commit()
+
+reader = csv.DictReader(open('c:\\temp\\filename.csv', 'r'), delimiter=',', quotechar='"')
+
+for row in reader:
+  if row['First Detected']:
+    temp = row['First Detected']
+    temp2 = temp[:10]
+    #cutting off HH:SS:MM
+    row['First Detected'] = datetime.datetime.strptime(temp2, "%m/%d/%Y").strftime("%Y-%m-%d")
+    
+    
